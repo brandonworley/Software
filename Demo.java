0afa03cfdf3b93b083e42ad67ef61471a0a0ad7a
brandonworley <worleyba@appstate.edu>
2024-08-21 10:45:46 -0400

    Fixed crash

--- Demo.java
+++ Demo.java
@@ -5,13 +5,22 @@ public class Demo
 
     public static void main (String[] args)
     {
+        int a = 0;
         Inheritor i = new Inheritor();
         Inheritor2 i2 = new Inheritor2();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string");
         String s = sc.nextLine();
         System.out.println("Do you want to send this to inheritor or inheritor2 (type 1 or 2)");
-        int a = sc.nextInt();
+        try
+        {
+            a = sc.nextInt();
+        }
+        catch (Exception e)
+        {
+            a = 1;
+        }
+        
         if (a == 1)
         {
             i.mystery(s);
